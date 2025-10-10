3338a78b370940825af83ab3f10b060be51eb50f
Baret <450955+Baret@users.noreply.github.com>
2025-10-10 18:12:36 +0200

    Draw my own texture with ColorfulBatch

--- core/src/main/kotlin/de/gleex/reqtrans/Main.kt
+++ core/src/main/kotlin/de/gleex/reqtrans/Main.kt
@@ -2,7 +2,8 @@ package de.gleex.reqtrans
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.github.tommyettinger.colorful.ipt.ColorfulBatch
+import com.github.tommyettinger.colorful.rgb.Palette
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
@@ -21,12 +22,13 @@ class Main : KtxGame<KtxScreen>() {
 }
 
 class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
-    private val batch = SpriteBatch()
+    private val image = Texture("spaceIcons/single/Retina/enemy_A.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+    private val batch = ColorfulBatch()
 
     override fun render(delta: Float) {
         clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
         batch.use {
+            it.setColor(Palette.YELLOW)
             it.draw(image, 100f, 160f)
         }
     }
